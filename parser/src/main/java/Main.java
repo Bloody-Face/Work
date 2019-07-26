2b6d1a438d187034cf7af48373105f3464c0b2fd
Александр <alek295@mail.ru>
2019-07-26 18:45:52 +0500

    xxx

--- parser/src/main/java/Main.java
+++ parser/src/main/java/Main.java
@@ -1,12 +1,54 @@
-import java.net.MalformedURLException;
+
 import java.net.URL;
+import java.io.IOException;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+
 public class Main {
     static void MyFun(){
         try {
             URL connect = new URL("http://www.cbr.ru/scripts/XML_daily.asp");
-            System.err.println(connect);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
+            DocumentBuilder documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            Document document = documentBuilder.parse(connect.openStream());
+
+
+            System.out.println(document.getPrefix());
+            System.out.println(document.getElementsByTagName("Date"));
+
+            Node root = document.getDocumentElement();
+            System.out.println("Курс валют:");
+            System.out.println();
+
+
+
+            //NodeList Lisps = root.getChildNodes();
+            /*for (int i = 0; i < Lisps.getLength(); i++) {
+                Node Lisp = Lisps.item(i);
+                if (Lisp.getNodeType() != Node.TEXT_NODE) {
+                    NodeList bookProps = Lisp.getChildNodes();
+                    for(int j = 0; j < bookProps.getLength(); j++) {
+                        Node bookProp = bookProps.item(j);
+                        if (bookProp.getNodeType() != Node.TEXT_NODE) {
+                            System.out.println(bookProp.getNodeName() + ":" + bookProp.getChildNodes().item(0).getTextContent());
+                        }
+                    }
+                    System.out.println("===========>>>>");
+                }
+            }*/
+
+
+        } catch (ParserConfigurationException ex) {
+            ex.printStackTrace(System.out);
+        } catch (SAXException ex) {
+            ex.printStackTrace(System.out);
+        } catch (IOException ex) {
+            ex.printStackTrace(System.out);
         }
 
     }
